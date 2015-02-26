1ba2f368358dd92eb5a66c26ec3d94239b0740a0
unknown <LiHa@5QHV2Q1.corp.root.nasd.com>
2015-02-26 15:03:53 -0500

    some comment and some minor logic change to reduce code duplicate

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
-
-/** Simple extension that adds a "File > Hello World" menu item */
 define(function (require, exports, module) {
     "use strict";
 
@@ -19,38 +15,31 @@ define(function (require, exports, module) {
         INDENT_KEY = "Ctrl-Shift-I",
         FORMAT_KEY = "Ctrl-Shift-F",
         MIN_KEY = "Ctrl-Shift-M";
+    // home grow format lib
+    var formatter = require("lib/format-helper");
 
     var supportedFile = {xml:true,css:true,sql:true,json:true};
+    var supportedAction = {INDENT:'indent', FORMAT:"format", MINIFY:"minify"};
 
     CommandManager.register("Auto Indent", INDENT_COMMAND_ID, autoIndent);
     CommandManager.register("Auto Format", FORMAT_COMMAND_ID, autoFormat);
     CommandManager.register("Auto Minify", MIN_COMMAND_ID, autoMin);
+    //do indent for the file content
     function autoIndent() {
-        var editor = EditorManager.getFocusedEditor();
-        if (!editor) {
-            return;
-        }
-        var doc = editor._codeMirror;
-        // var e = doc.fromTextArea(textarea.get(0), {});
-        for (var i=0;i<doc.lineCount();i++) { 
-            doc.indentLine(i); 
-        }
+        modifyContent(supportedAction.INDENT);
     };
 
-    var formatter = require("lib/format-helper");
+    // do format for the content
     function autoFormat(){
-        var editor = EditorManager.getFocusedEditor();
-        var file = DocumentManager.getCurrentDocument().file;
-        var extention = FileUtils.getFileExtension(file.fullPath);
-        if (!editor || !extention) {
-            return;
-        }
-        var doc = editor._codeMirror;
-        var options = {method: extention};
-        doc.setValue(formatter.doFormat(doc.getValue(), options));
+        modifyContent(supportedAction.FORMAT);
     };
 
+    //do minify for the content
     function autoMin(){
+        modifyContent(supportedAction.MINIFY);
+    };
+
+    function modifyContent(action){
         var editor = EditorManager.getFocusedEditor();
         var file = DocumentManager.getCurrentDocument().file;
         var extention = FileUtils.getFileExtension(file.fullPath);
@@ -58,11 +47,25 @@ define(function (require, exports, module) {
             return;
         }
         var doc = editor._codeMirror;
-        var options = {method: extention+'min'};
-        doc.setValue(formatter.doFormat(doc.getValue(), options));
-    };
 
+        switch(action){
+            case supportedAction.INDENT:
+                for (var i=0;i<doc.lineCount();i++) { 
+                    doc.indentLine(i); 
+                }
+                break;
+            case supportedAction.FORMAT:
+                doc.setValue(formatter.doFormat(doc.getValue(), {method: extention}));
+                break;
+            case supportedAction.MINIFY:
+                doc.setValue(formatter.doFormat(doc.getValue(), {method: extention+'min'}));
+                break;
+            default:
+                //do nothing
+        }
+    }
 
+    // handling file selection change to show/hide format/minify menu
     var fileChangeHandler = function ($event, newFile, newPaneId, oldFile, oldPaneId) {
         var newExt = FileUtils.getFileExtension(newFile.fullPath);
         // on app init, old file is null.
@@ -76,30 +79,29 @@ define(function (require, exports, module) {
         }
         else{
             var oldExt = FileUtils.getFileExtension(oldFile.fullPath);
-            if(newExt !== oldExt){
-                if(supportedFile[oldExt]){
-                    menu.removeMenuItem(FORMAT_COMMAND_ID);
-                    menu.removeMenuItem(MIN_COMMAND_ID);
-                    KeyBindingManager.removeBinding(FORMAT_KEY);
-                    KeyBindingManager.removeBinding(MIN_KEY);
-                }
-                if(supportedFile[newExt]){
-                    menu.addMenuItem(FORMAT_COMMAND_ID, [{key: FORMAT_KEY, platform: "win"},
-                                                         {key: FORMAT_KEY, platform: "mac"}]);
-                    menu.addMenuItem(MIN_COMMAND_ID, [{key: MIN_KEY, platform: "win"},
-                                                      {key: MIN_KEY, platform: "mac"}]);
-                }
+            // if new file is not supported and we have the menus, remove them.
+            if(supportedFile[oldExt] && !supportedFile[newExt]){
+                menu.removeMenuItem(FORMAT_COMMAND_ID);
+                menu.removeMenuItem(MIN_COMMAND_ID);
+                KeyBindingManager.removeBinding(FORMAT_KEY);
+                KeyBindingManager.removeBinding(MIN_KEY);
             }
-
+            // if old file is not supported but new file is,  we add the menus.
+            if(!supportedFile[oldExt] && supportedFile[newExt]){
+                menu.addMenuItem(FORMAT_COMMAND_ID, [{key: FORMAT_KEY, platform: "win"},
+                                                     {key: FORMAT_KEY, platform: "mac"}]);
+                menu.addMenuItem(MIN_COMMAND_ID, [{key: MIN_KEY, platform: "win"},
+                                                  {key: MIN_KEY, platform: "mac"}]);
+            }
         }
     };
 
     AppInit.appReady(function () {
+        //bind the file change event so that we could add/remove the format/minify based on file extension.
         $(MainViewManager).on('currentFileChange', fileChangeHandler);
     });
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
+    // bind indent command since it is for all file type;
     var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
     menu.addMenuItem(INDENT_COMMAND_ID, [{key: INDENT_KEY, platform: "win"},
                                          {key: INDENT_KEY, platform: "mac"}]);
